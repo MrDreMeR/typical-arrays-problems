5c9d7bf8fa72b8728167940335ea468950037815
Nikita Kryzhevich <Alex-55568@mail.ru>
2021-03-14 15:42:40 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
+  if(!array || array.join("").length ===0){
   return 0;
+  } 
+  let min =array [0];
+  array.forEach(item => {
+    if(item < min){
+    min= item;
+    }
+  })
+  return min;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.join("").length ===0){
+    return 0;
+    }
+  let max =array [0];
+  array.forEach(item => {
+    if(item > max){
+    max= item;
+    }
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.join("").length ===0){
+    return 0;
+    }
+    let avg = 0;
+    array.forEach(item => {
+     avg += item
+    })
+    return avg / array.length;
 }
